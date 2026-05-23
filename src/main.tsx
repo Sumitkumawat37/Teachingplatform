3ab5498031c26da84206b775ab8c80fd94cbbcbb
Khandelwal0304 <khandelwaljenendra966331@gmail.com>
2026-05-23 22:49:08 +0530

    Fix black screen on Vercel: remove GitHub Pages redirect logic from main.tsx and index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-// Handle GitHub Pages redirect
-if (window.location.pathname !== '/' && !window.location.pathname.includes('.')) {
-  const search = window.location.search;
-  const hash = window.location.hash;
-  const newPath = window.location.pathname.replace(/\/$/, '');
-  
-  if (search.includes('p=/')) {
-    const redirectPath = search.split('p=/')[1].split('&')[0];
-    window.history.replaceState(null, '', redirectPath + hash);
-  }
-}
-
 createRoot(document.getElementById("root")!).render(<App />);
