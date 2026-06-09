a7bf68c2e20edadcd7a765822dbdb49baed107f9
Khandelwal0304 <khandelwaljenendra966331@gmail.com>
2026-06-09 23:35:11 +0530

    Updates

--- assets/label-CgvO16vm.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as r,bi as a}from"./react-vendor-_0BIFGhU.js";import{t as i,z as l}from"./index-BKeWSEOU.js";const m=l("text-sm font-medium leading-none text-gray-300 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),d=o.forwardRef(({className:e,...s},t)=>r.jsx(a,{ref:t,className:i(m(),e),...s}));d.displayName=a.displayName;export{d as L};
