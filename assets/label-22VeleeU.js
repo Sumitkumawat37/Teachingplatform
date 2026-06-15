8de1d0f789d95ec1609a1e59df3adea22105adf9
Khandelwal0304 <khandelwaljenendra966331@gmail.com>
2026-06-16 04:58:49 +0530

    Updates

--- assets/label-22VeleeU.js
+++
@@ -1,1 +0,0 @@
-import{r as o,j as r,bk as a}from"./react-vendor-BXXAhQ-M.js";import{t as i,z as l}from"./index-BtPEzm2u.js";const m=l("text-sm font-medium leading-none text-gray-300 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),d=o.forwardRef(({className:e,...s},t)=>r.jsx(a,{ref:t,className:i(m(),e),...s}));d.displayName=a.displayName;export{d as L};
